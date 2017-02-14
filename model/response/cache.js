a7cd812e1fad3be1bea9abccd75f9d969e11d229
Tom Bailey <tombailey1@hotmail.co.uk>
2017-02-14 00:15:18 +0000

    Update cache to use days

--- model/response/cache.js
+++ model/response/cache.js
@@ -1,11 +1,12 @@
 /*jshint esversion: 6 */
 const SECS_IN_MINUTE = 60;
 const SECS_IN_HOUR = 60 * SECS_IN_MINUTE;
+const SECS_IN_DAY = 24 * SECS_IN_HOUR;
 
 const times = (modifier) => {
   return {
-    "hours": (x) => {
-      return modifier + ", max=" + (SECS_IN_HOUR * x);
+    "days": (x) => {
+      return modifier + ", max=" + (SECS_IN_DAY * x);
     }
   };
 };
