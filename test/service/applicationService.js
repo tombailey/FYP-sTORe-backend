7ac3b2bdf9207f48d5d6c3b5f38ed4d177499bb6
Tom Bailey <tombailey1@hotmail.co.uk>
2017-02-26 21:26:51 +0000

    Add test WIP

---
+++ test/service/applicationService.js
@@ -0,0 +1,175 @@
+/*jshint esversion: 6 */
+describe("tests", () => {
+  const expect = require("chai").expect;
+
+  const error = require("../../model/response/error");
+
+  describe("applicationService", () => {
+    describe("#badRequest", () => {
+      it("should produce 400 error code", () => {
+        //arrange
+        var mockResponse = {
+          "json": () => {
+
+          },
+          "status": (code) => {
+            expect(code).to.equal(400);
+            return mockResponse;
+          }
+        };
+        var message = "bad request";
+
+
+        //act
+        error.badRequest(mockResponse, message);
+
+
+        //assert
+
+      });
+    });
+  });
+
+  describe("error", () => {
+    describe("#badRequest", () => {
+      it("should produce correct error json", () => {
+        //arrange
+        var message = "bad request";
+
+        var mockResponse = {
+          "json": (data) => {
+            expect(data).to.deep.equal({
+              "errors": [{
+                "code": 400,
+                "message": message
+              }]
+            });
+          },
+          "status": (code) => {
+            return mockResponse;
+          }
+        };
+
+        //act
+        error.badRequest(mockResponse, message);
+
+
+        //assert
+
+      });
+    });
+  });
+
+  describe("error", () => {
+    describe("#notFound", () => {
+      it("should produce 404 error code", () => {
+        //arrange
+        var mockResponse = {
+          "json": () => {
+
+          },
+          "status": (code) => {
+            expect(code).to.equal(404);
+            return mockResponse;
+          }
+        };
+        var message = "not found";
+
+
+        //act
+        error.notFound(mockResponse, message);
+
+
+        //assert
+
+      });
+    });
+  });
+
+  describe("error", () => {
+    describe("#notFound", () => {
+      it("should produce correct error json", () => {
+        //arrange
+        var message = "not found";
+
+        var mockResponse = {
+          "json": (data) => {
+            expect(data).to.deep.equal({
+              "errors": [{
+                "code": 404,
+                "message": message
+              }]
+            });
+          },
+          "status": (code) => {
+            return mockResponse;
+          }
+        };
+
+        //act
+        error.notFound(mockResponse, message);
+
+
+        //assert
+
+      });
+    });
+  });
+
+  describe("error", () => {
+    describe("#internalServerError", () => {
+      it("should produce 500 error code", () => {
+        //arrange
+        var mockResponse = {
+          "json": () => {
+
+          },
+          "status": (code) => {
+            expect(code).to.equal(500);
+            return mockResponse;
+          }
+        };
+        var message = "internal server error";
+
+
+        //act
+        error.internalServerError(mockResponse, message);
+
+
+        //assert
+
+      });
+    });
+  });
+
+  describe("error", () => {
+    describe("#internalServerError", () => {
+      it("should produce correct error json", () => {
+        //arrange
+        var message = "internal server error";
+
+        var mockResponse = {
+          "json": (data) => {
+            expect(data).to.deep.equal({
+              "errors": [{
+                "code": 500,
+                "message": message
+              }]
+            });
+          },
+          "status": (code) => {
+            return mockResponse;
+          }
+        };
+
+        //act
+        error.internalServerError(mockResponse, message);
+
+
+        //assert
+
+      });
+    });
+  });
+
+});
