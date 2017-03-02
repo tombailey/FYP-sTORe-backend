38f1b792f28f1de2dbf67a1e43d3db8ff7f2882e
Tom Bailey <tombailey1@hotmail.co.uk>
2017-03-02 11:20:57 +0000

    Add option to cache responses

--- model/response/success.js
+++ model/response/success.js
@@ -1,13 +1,17 @@
 /*jshint esversion: 6 */
-const ok = (res, data) => {
-  success(res, 200, data);
+const ok = (res, data, cache) => {
+  success(res, 200, data, cache);
 };
 
-const created = (res, data) => {
-  success(res, 201, data);
+const created = (res, data, cache) => {
+  success(res, 201, data, cache);
 };
 
-const success = (res, status, data) => {
+const success = (res, status, data, cache) => {
+  if (cache) {
+    res.set("Cache-control", cache);
+  }
+
   res.status(status).json({
     "data": data
   });
