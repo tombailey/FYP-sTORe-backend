c0c0742be759308596ccd06050bf7c73f51fd023
Tom Bailey <tombailey1@hotmail.co.uk>
2017-03-08 19:22:31 +0000

    Fix undefined developerId variable

--- model/endpoint/application.js
+++ model/endpoint/application.js
@@ -126,7 +126,7 @@ module.exports = (app, upload, mongoose, entities, storageBucket) => {
 
     sessionService.getSession(sessionId).then((session) => {
       //TODO: check for duplicate package name
-      return applicationService.create(name, description, [category], developerId);
+      return applicationService.create(name, description, [category], session.developer.id);
     }).then((newApplication) => {
       success.created(res, {
         "registered": true
