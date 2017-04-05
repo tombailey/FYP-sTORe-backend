47e977e39f6d9b8cf888df865ad48921eb04f252
Tom Bailey <tombailey1@hotmail.co.uk>
2017-04-05 15:15:36 +0100

    Add developer registration page

---
+++ public/scripts/request.js
@@ -0,0 +1,51 @@
+window.request = function(url) {
+  var handleResponse = function(xHttp, callback) {
+    xHttp.onreadystatechange = function() {
+      if (xHttp.readyState == 4) {
+        if (xHttp.status >= 200 && xHttp.status < 300) {
+          callback(false, xHttp.status, xHttp.responseText);
+        } else {
+          callback(true, xHttp.status, xHttp.responseText);
+        }
+      }
+    };
+  };
+
+  var get = function() {
+    var xHttp = new XMLHttpRequest();
+    xHttp.open("GET", url, true);
+    xHttp.send();
+
+    return {
+      "then": function(callback) {
+        handleResponse(xHttp, callback);
+      }
+    };
+  };
+
+  var post = function(params) {
+    var xHttp = new XMLHttpRequest();
+    xHttp.open("POST", url, true);
+    xHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    var body = "";
+    params.forEach(function(param, index) {
+      body += param.name + "=" + param.value;
+      if (index != params.length - 1) {
+        body += "&";
+      }
+    });
+    xHttp.send(body);
+
+    return {
+      "then": function(callback) {
+        handleResponse(xHttp, callback);
+      }
+    };
+  };
+
+  return {
+    "get": get,
+    "post": post
+  };
+};
