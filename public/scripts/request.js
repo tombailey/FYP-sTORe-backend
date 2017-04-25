12ecc08ed49339e3f0c8315e43d4670a93a80287
Tom Bailey <tombailey1@hotmail.co.uk>
2017-04-26 00:12:47 +0100

    Add edit screenshot

--- public/scripts/request.js
+++ public/scripts/request.js
@@ -24,25 +24,54 @@ window.request = function(url) {
   };
 
   var body = function(method) {
-    return function(params) {
-      var xHttp = new XMLHttpRequest();
-      xHttp.open(method, url, true);
-      xHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    return function(params, isMultipart) {
+      if (isMultipart) {
+        return multipart(method, params);
+      } else {
+        return urlencoded(method, params);
+      }
+    };
+  };
 
-      var body = "";
-      params.forEach(function(param, index) {
-        body += param.name + "=" + param.value;
-        if (index != params.length - 1) {
-          body += "&";
-        }
-      });
-      xHttp.send(body);
+  var urlencoded = function(method, params) {
+    var xHttp = new XMLHttpRequest();
+    xHttp.open(method, url, true);
+    xHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-      return {
-        "then": function(callback) {
-          handleResponse(xHttp, callback);
-        }
-      };
+    var body = "";
+    params.forEach(function(param, index) {
+      body += param.name + "=" + param.value;
+      if (index != params.length - 1) {
+        body += "&";
+      }
+    });
+    xHttp.send(body);
+
+    return {
+      "then": function(callback) {
+        handleResponse(xHttp, callback);
+      }
+    };
+  };
+
+  var multipart = function(method, params) {
+    var xHttp = new XMLHttpRequest();
+    xHttp.open(method, url, true);
+
+    var formData = new FormData();
+    params.forEach(function(param, index) {
+      if (param.filename === undefined) {
+        formData.append(param.name, param.value);
+      } else {
+        formData.append(param.name, param.value, param.filename);
+      }
+    });
+    xHttp.send(formData);
+
+    return {
+      "then": function(callback) {
+        handleResponse(xHttp, callback);
+      }
     };
   };
 
